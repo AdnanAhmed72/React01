2a49f6ec75feed384b64fb21a0c66876bd1887d6
AdnanAhmed72 <125689829+AdnanAhmed72@users.noreply.github.com>
2023-07-07 16:51:59 +0530

    React2.4

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,8 +1,20 @@
 import './ExpenseForm.css'
+import React, {useState} from 'react'
 export default function ExpenseForm(){
+    const [enteredTitle,setEnteredTitle] = useState('')
+    const [enteredAmount,setEnteredAmount] = useState('')
+    const [enteredDate,setEnteredDate] = useState('')
     const titleChangeHandler = (e) =>{
-        console.log(e.target.value)
-        
+        setEnteredTitle(e.target.value)
+        //console.log(e.target.value)
+    }
+    const amountChangeHandler=(e)=>{
+        //console.log(e.target.value)
+        setEnteredAmount(e.target.value)
+    }
+    const dateChangeHandler=(e)=>{
+        setEnteredDate(e.target.value)
+        //console.log(e.target.value)
     }
     return(
         <form>
@@ -13,11 +25,11 @@ export default function ExpenseForm(){
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' step="0.01" min="0.01"/>
+                    <input type='number' step="0.01" min="0.01" onChange={amountChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min="2019-01-01" max="2022-12-31"/>
+                    <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className='new-expense__actions'>
