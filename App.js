ad9fac7f04c6062617e9365315b2475d6c4e0cae
AdnanAhmed72 <125689829+AdnanAhmed72@users.noreply.github.com>
2023-07-24 21:08:29 +0530

    React3.6

--- App.js
+++ App.js
@@ -1,41 +1,18 @@
-import Expenses from "./components/ExpensesList/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
-import React,{useState} from "react";
+import React,{ useState } from 'react';
+import AddUsers from './Components/Users/AddUsers';
+import UsersList from './Components/Users/UsersList';
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-  const [expenseList,setExpenseList] = useState(expenses)
-  const addExpenses =(newData) =>{
-    const newExpense = {...newData}
-    // console.log('In App.js')
-    console.log(newExpense)
-    
-    setExpenseList([newExpense,...expenseList])
+  const [userList,setUserList] = useState([])
+  const addUserHandler = (uName,uAge) =>{
+    setUserList((prevList)=>{
+      return [...prevList,{name:uName,age:uAge,id:Math.random().toString()}]
+    })
   }
   return (
-    <div>
-      <NewExpense onAddExpense={addExpenses}/>
-      <Expenses expenses = {expenseList}></Expenses>
-    </div>
+    <React.Fragment>
+      <AddUsers onAddUsers={addUserHandler}/>
+      <UsersList users={userList}/>
+    </React.Fragment>
   );
 }
 
