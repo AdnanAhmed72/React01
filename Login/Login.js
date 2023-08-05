dff1dce731737ad37db331f3c65b7ad9fe58a9c6
AdnanAhmed72 <125689829+AdnanAhmed72@users.noreply.github.com>
2023-08-05 11:02:12 +0530

    React3.10

---
+++ Login/Login.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useState,useReducer,useContext } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+import AuthContext from '../store/auth-context';
+import Input from '../Input/Input';
+
+const emailReducer = (state,action) =>{
+  if(action.type === 'USER_INPUT'){
+    return {value:action.val,isValid:action.val.includes('@')};
+  }
+  if(action.type === 'INPUT_BLUR'){
+    return {value:state.value,isValid:state.value.includes('@')};
+  }
+  //return {value:'',isValid:false};
+}
+const passwordReducer = (state,action) =>{
+  if(action.type === 'INPUT_PASSWORD'){
+    return {value:action.val,isValid:action.val.length > 6}
+  };
+  if(action.type === 'INPUT_BLUR'){
+    return {value:state.value,isValid:state.value.length > 6}
+  };
+}
+
+const Login = (props) => {
+
+  const [enteredColName,setEnteredColName] = useState('');
+  const [collegeIsValid,setColIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const authCtx = useContext(AuthContext)
+
+  const [emailState, dispatcEmail] = useReducer(emailReducer,
+    {value:'',isValid:null});
+  const [passwordState,dispatchPassword] = useReducer(passwordReducer,
+    {value:'',isValid:null});
+
+  useEffect(()=>{
+    const identifier = setTimeout(() => {
+      console.log('Checking Form Validity');
+      setFormIsValid(
+      emailState.isValid && passwordState.isValid)
+    }, 500);
+    return () =>{
+      console.log('CLEANUP')
+      clearTimeout(identifier)
+    }
+  },[emailState,passwordState]);
+
+  const emailChangeHandler = (event) => {
+    dispatcEmail({type:'USER_INPUT',val:event.target.value})
+
+    // setFormIsValid(
+    //   event.target.value.includes('@') && enteredPassword.trim().length > 6 
+    //   && enteredColName.trim().length > 3
+    // );
+  };
+
+  const passwordChangeHandler = (event) => {
+    dispatchPassword({type:'INPUT_PASSWORD',val:event.target.value})
+
+    // setFormIsValid(
+    //   event.target.value.trim().length > 6 && enteredEmail.includes('@')
+    // );
+  };
+
+  const colNameChangeHandler = (event) =>{
+    setEnteredColName(event.target.value);
+
+    setFormIsValid(
+      event.target.value.trim().length > 3 && emailState.isValid
+      && passwordState.isValid.length > 6
+    );
+  }
+
+  const validateEmailHandler = () => {
+    dispatcEmail({type:'INPUT_BLUR'})
+  };
+
+  const validatePasswordHandler = () => {
+    dispatchPassword({type:'INPUT_BLUR'})
+  };
+
+  const validateColNameHandler = () =>{
+    setColIsValid(enteredColName.trim().length > 3);
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    authCtx.onLogin(emailState.value, passwordState.value);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <Input id="email" type="email" 
+        label="E-Mail" value={emailState.value}
+        onChange={emailChangeHandler} onBlur={validateEmailHandler}
+        isValid={emailState.isValid}/>
+        <Input id="password" type="password" 
+        label="Password" value={passwordState.value}
+        onChange={passwordChangeHandler} onBlur={validatePasswordHandler}
+        isValid={passwordState.isValid}/>
+      
+        <div className={`${classes.control} ${
+            collegeIsValid === false ? classes.invalid : ''
+          }`}>
+          <label htmlFor="college">College Name</label>
+            <input
+              type="text"
+              id="college"
+              value={enteredColName}
+              onChange={colNameChangeHandler}
+              onBlur={validateColNameHandler}/>
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
